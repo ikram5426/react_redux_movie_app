3c5a926e9c2e09759c56195f9a53cb360ef9009e
IkramKhan <ikram5426@gmail.com>
2023-04-28 02:57:28 +0500

    routes add for all main pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
 import './App.scss';
+import { Routes,Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import MovieDetail from './components/MovieDetail/MovieDetail';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div >
-  App
+    <div className='app'>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movie/:imdbID' element={<MovieDetail/>} />
+        <Route path='*' element={<PageNotFound/>}/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
